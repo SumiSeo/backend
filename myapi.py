9d29a826e2f491faa741842303288a25400f0ef7
Sumi <qkobr94@gmail.com>
2025-08-12 11:21:43 +0200

    feat/create-query-and-path-params-in-endpoint

--- myapi.py
+++ myapi.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI()
 
@@ -6,6 +6,21 @@ app = FastAPI()
 # localhost/delete-user
 # localhost/create-user
 # GET 
+
+students = { 
+    1: {
+        "name" : "john",
+        "age" : 17,
+        "class":"year 12"
+    },
+    2: {
+        "name" : "sam",
+        "age" : 16,
+        "class":"year 11"
+    }
+}
+
+
 @app.get("/")
 def index():
     return { "name" : "Sumi Seo" }
@@ -16,6 +31,18 @@ def index():
     return { "name" : "Sum Sum" }
 
 
+@app.get("/get-student/{student_id}")
+def get_student(student_id: int = Path(..., description="The ID of the student that you want to view.", gt=0)):
+    return students[student_id]
+    
+
+# do not define with query parameter
+@app.get("/get-by-name")
+def get_student(name: str):
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]
+    return {"Data": "Not found"}
 # POST
 # PUT
 # DELETE
